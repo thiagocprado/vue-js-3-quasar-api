84bf16f85d8cdfc9bf7d9097043ed8cf87e354e7
thiago prado <thiago.prado@multilaser.com.br>
2022-07-25 15:07:43 -0300

    criando métodos de requisiição

--- quasar.config.js
+++ quasar.config.js
@@ -10,10 +10,10 @@
 
 const { configure } = require('quasar/wrappers')
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function (ctx) {
   return {
     eslint: {
-      // fix: true,
+      fix: true,
       // include = [],
       // exclude = [],
       // rawOptions = {},
@@ -27,15 +27,10 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
-    boot: [
-
-      'axios'
-    ],
+    boot: ['axios'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
-    css: [
-      'app.css'
-    ],
+    css: ['app.css'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -58,7 +53,10 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'hash' // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      env: {
+        API_URL: ctx.dev ? 'http://localhost:3000/' : 'http://localhost:3000/'
+      }
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -85,6 +83,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      port: 8080,
       open: true // opens browser window automatically
     },
 
@@ -103,7 +102,10 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify',
+        'Dialog'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
@@ -177,13 +179,11 @@ module.exports = configure(function (/* ctx */) {
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
@@ -197,9 +197,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-browser-extensions/configuring-bex
     bex: {
-      contentScripts: [
-        'my-content-script'
-      ]
+      contentScripts: ['my-content-script']
 
       // extendBexScriptsConf (esbuildConf) {}
       // extendBexManifestJson (json) {}
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,76 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-table title="Treats" :rows="posts" :columns="columns" row-key="name">
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn
+            @click="handleDeletePost(props.row.id)"
+            icon="delete"
+            color="negative"
+            dense
+            size="sm"
+          />
+        </q-td>
+      </template>
+    </q-table>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+import { useQuasar } from 'quasar'
+import postsService from 'src/services/posts'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup () {
+    const posts = ref([])
+    const { list, remove } = postsService()
+    const columns = [
+      { name: 'id', field: 'id', label: 'Id', sortable: true, align: 'left' },
+      { name: 'title', field: 'title', label: 'Título', sortable: true, align: 'left' },
+      { name: 'author', field: 'author', label: 'Autor', sortable: true, align: 'left' },
+      { name: 'actions', field: 'actions', label: 'Ações', sortable: true, align: 'right' }
+
+    ]
+
+    onMounted(() => {
+      getPosts()
+    })
+
+    const $q = useQuasar()
+
+    const getPosts = async () => {
+      try {
+        const data = await list()
+        posts.value = data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    const handleDeletePost = (id) => {
+      try {
+        $q.dialog({
+          title: 'Apagar',
+          message: 'Deseja apagar esse post?',
+          cancel: true,
+          persistent: true
+        }).onOk(async () => {
+          await remove(id)
+          $q.notify({ message: 'Apagado com sucesso', icon: 'check', color: 'positive' })
+          await getPosts()
+        })
+      } catch (error) {
+        $q.notify({ message: 'Erro ao apagar', icon: 'times', color: 'negative' })
+      }
+    }
+
+    return {
+      posts,
+      columns,
+      handleDeletePost
+    }
+  }
 })
 </script>
